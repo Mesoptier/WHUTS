f4285eddf3c120cab5588a31c9620686e4bbb996
Koen Klaren <koen.klaren@gmail.com>
2021-05-15 12:59:08 +0200

    Generate all tile orientations

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,34 @@ const TILE_SIZE: usize = 2;
 type Tile = [[bool; TILE_SIZE]; TILE_SIZE];
 
 fn main() {
+    // Create the base tile
     let mut tile: Tile = [[false; TILE_SIZE]; TILE_SIZE];
     tile[0][0] = true;
     tile[0][1] = true;
     tile[1][0] = true;
+    let tile = tile;
 
-    print_tile(&tile);
-
+    // Generate all orientations of the given tile
     let mut tile_orientations: HashSet<Tile> = HashSet::new();
-    tile_orientations.insert(tile);
+    for &fx in &[false, true] {
+        for &fy in &[false, true] {
+            let mut new_tile = tile;
 
+            if fx {
+                new_tile.reverse();
+            }
+            if fy {
+                for x in 0..TILE_SIZE {
+                    new_tile[x].reverse();
+                }
+            }
 
+            // print_tile(&new_tile);
+            // println!();
+
+            tile_orientations.insert(new_tile);
+        }
+    }
 }
 
 fn print_tile(tile: &Tile) {
